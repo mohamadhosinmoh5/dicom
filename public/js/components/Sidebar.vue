dae0fbc7677f033d295755c97a2fea8bca396261
mahlakhalili <mahlakhalili1@gmail.com>
2023-10-04 13:31:55 +0330

    icon

--- public/js/components/Sidebar.vue
+++ public/js/components/Sidebar.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="page-link-containers row-[1/3] py-10 flex flex-col gap-5 w-full h-full">
+    <div class="page-link-containers row-[1/3] py-10 flex flex-col gap-5 w-full h-full gap-[25px]">
         <router-link
             v-for="link in sidebarLinks"
             :key="link"
             :to="link.to"
-            class="side-bar-link relative flex items-center gap-4 w-full h-9 text-base font-bold text-powderBlue pr-[12%] hover:text-[#6E80B7]"
+            class="side-bar-link relative flex items-center gap-4 w-full h-9 text-base font-bold text-powderBlue pr-[12%] hover:text-[#6E80B7] ml-[20px] "
         >
-            {{ link.text }}
+            <img
+                :src = "link.imgURL" alt=""
+                class=" w-[50px] h-[50px]"
+                >
+                {{ link.text }}
         </router-link>
     </div>
 </template>
@@ -17,29 +21,44 @@ export default {
         return {
             sidebarLinks: [
                 {
-                    to: "/dicom/public/dicom-viewer",
-                    text: "DicomViewer",
+                    to     : "/dicom/public/image-viewer",
+                    text   : "DicomViewer",
+                    imgURL : "../../assets/pics/imageIcon.png"
                 },
                 {
-                    to: "/dicom/public/three-d-viewer",
-                    icon: "shopping_cart",
-                    text: "ThreeDViewer",
-                },
-                {
-                    to: "/dicom/public/compare-data",
-                    text: "CompareData",
-                },
-                {
-                    to: "/dicom/public/generate-report",
-                    text: "GenerateReport",
+                    to     : "/dicom/public/three-d-viewer",
+                    text   : "ThreeDViewer",
+                    imgURL : "../../assets/pics/3DIcon.png"
                 },
                 {
                     to: "/dicom/public/table-graph",
                     text: "TableGraph",
+                    imgURL : "../../assets/pics/chartIcon.png"
+                },
+                {
+                    to: "/dicom/public/compare-data",
+                    text: "CompareData",
+                    imgURL : "../../assets/pics/compareIcon.png"
+                },
+                {
+                    to: "/dicom/public/generate-report",
+                    text: "GenerateReport",
+                    imgURL : "../../assets/pics/reportIcon.png"
                 },
                 {
                     to: "/dicom/public/settings",
                     text: "Settings",
+                    imgURL : "../../assets/pics/settingsIcon.png"
+                },
+                {
+                    to: "/dicom/public/add",
+                    text: "Add",
+                    imgURL : "../../assets/pics/addIcon.png"
+                },
+                {
+                    to: "/dicom/public/analysis",
+                    text: "Analysis",
+                    imgURL : "../../assets/pics/analysisIconn.png"
                 },
                 {
                     to: "/dicom/public/log-in",
