e5880d1e520fd10c26c1762864de0141fa6b298d
MRAlirad <m.r.alirad75@gmail.com>
2023-10-22 08:44:08 +0330

    pdf parser

--- public/js/pages/GenerateReport.vue
+++ public/js/pages/GenerateReport.vue
@@ -1,13 +1,48 @@
 <template>
-  generate
+    <div class="wow">
+        <div id="appreport" ref="document">
+            <div id="element-to-convert">
+                <center>
+                    <img width="400" src="../20220114_091418.jpg" />
+                    <img width="400" src="../20220114_091418.jpg" />
+                    <img width="400" src="../20220114_091418.jpg" />
+                    <img width="400" src="../20220114_091418.jpg" />
+                    <img width="400" src="../20220114_091418.jpg" />
+                    <img width="400" src="../20220114_091418.jpg" />
+                </center>
+            </div>
+        </div>
+        <Button text="Export to PDF" @on-click="exportToPDF" />
+    </div>
 </template>
 
 <script>
+import html2pdf from "html2pdf.js";
+import Button from "../components/Button.vue";
 export default {
-
-}
+    components: {
+        Button,
+    },
+    methods: {
+        exportToPDF() {
+            html2pdf(document.getElementById("element-to-convert"));
+        },
+    },
+};
 </script>
 
-<style>
+<style scoped>
+#appreport {
+    margin-top: 60px;
+}
 
+.wow {
+    height: 70vh;
+    overflow: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 50px;
+}
 </style>
