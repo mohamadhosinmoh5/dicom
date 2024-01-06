f2246f3a8e4f415ed0f4f1ebb32cb89bfff35371
MRAlirad <m.r.alirad75@gmail.com>
2024-01-06 21:09:52 +0330

    Update DicomViewer.vue

--- public/js/pages/DicomViewer.vue
+++ public/js/pages/DicomViewer.vue
@@ -16,19 +16,42 @@
 		props: ['src'],
 		data() {
 			return {
+				iframe: undefined,
 				dicomInput: undefined,
+				loadCount: 0,
 			};
 		},
-		mounted() {
-			setTimeout(() => {
-				const iframe = document.getElementById('dicomIframe');
-				this.dicomInput = iframe.contentWindow.document.getElementById('filebutton');
-			}, 5000);
+		async mounted() {
+			// show loader
+			await this.loadIframe();
+			// hide loader
 		},
+		watch: {},
 		methods: {
-			addDicom() {
-				console.log(this.dicomInput);
-				this.dicomInput?.click();
+			async addDicom() {
+				await this.loadIframe();
+				if (this.loadCount > 0) {
+					this.iframe.contentWindow.location.reload();
+					setTimeout(() => {
+						this.dicomInput?.click();
+					}, 100);
+				} else {
+					// console.log(this.dicomInput);
+					this.dicomInput?.click();
+					this.loadCount++;
+				}
+			},
+			async loadIframe() {
+				this.iframe = document.getElementById('dicomIframe');
+				let iframeDoc = this.iframe.contentDocument || this.iframe.contentWindow.document;
+
+				// Check if loading is complete
+				if (iframeDoc.readyState == 'complete') {
+					//iframe.contentWindow.alert("Hello");
+					this.iframe.contentWindow.onload = async ()=>  {
+						this.dicomInput = this.iframe.contentWindow.document.getElementById('filebutton');
+					};
+				}
 			},
 		},
 	};
